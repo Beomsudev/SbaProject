2b2d4bf4b28707ba679489b00039aa635c89c906
Beomsudev <bumsu@naver.com>
2020-09-21 16:41:11 +0900

    modify file

--- contacts/contacts.py
+++ contacts/contacts.py
@@ -1,61 +1,66 @@
 class Contact:
-    def _init__(self, name , phone, email, addr):
-        self.name = name    # self.name과 name은 서로 다른 존재 , 할당함으로써 연결됨
+    def __init__(self, name, phone, email, addr):
+      # self.name과 name은 서로 다른 존재인데, 할당함으로써 연결됨.
+        self.name = name
         self.phone = phone
         self.email = email
         self.addr = addr
 
     def print_info(self):
-        print(f'이름: {self.name}, 전화번호 {self.phone}, 이메일: {self.email}, 주소: {self.addr}')
-        # self.name과 name은 서로 다른 존재 이므로{self.name} 과 {name} 은 서로 다른 상태이다.
+        print(
+            f'이름: {self.name}, 전화번호: {self.phone}, 이메일: {self.email}, 주소: {self.addr}')
+
     @staticmethod
     def set_contact():
-        name = input('이름') # 나머지 완성
-        phone = input('전화번호') 
-        email = input('이메일') 
-        addr = input('주소') 
-
-        contact = Contact(name, phone, email, addr) # Contact는 클래스명, contact는 인스턴스명 / 대소문자로 구분해 주는 것 권장
+        name = input('이름')  # self.name과 name은 서로 다름
+        phone = input('전화번호')
+        email = input('이메일')
+        addr = input('주소')
+        contact = Contact(name, phone, email, addr)
+        # Contact는 클래스명, contact는 인스턴스명이 된다.
         return contact
-    
+
     @staticmethod
     def get_contact(clist):
         for i in clist:
-            i.print_info()  # 8번라인에 선언된 메소드(클래스 내부에 선언된 함수)
+            i.print_info()
+            # 9번라인에 선언된 메소드(클래스 내부에 선언된 함수)
+            # 함수는 클래스 밖에 있는 것
 
     @staticmethod
     def del_contact(clist, name):
-        for i,t in enumerate(clist):    # i = index, t = 요소(인스턴스)를 출력한다 의 의미
+        for i, t in enumerate(clist):  # i는 인덱스, t는 요소(인스턴스)를 뽑는다.
             if t.name == name:
                 del clist[i]
 
     @staticmethod
     def print_menu():
-        print('1 연락처 입력')    
+        print('1 연락처 입력')
         print('2 연락처 출력')
         print('3 연락처 삭제')
         print('4 종료')
-        menu = input('메뉴선택:')
+        menu = input('메뉴 선택: ')
         return menu
 
     @staticmethod
     def run():
-        clist =[]    
+        clist = []
         while 1:
             menu = Contact.print_menu()
-            if menu =='1':
-                t =Contact.print_menu()
+            if menu == '1':
+                t = Contact.set_contact()
                 clist.append(t)
-            if menu =='2':
-                pass # 직접 완성
-            if menu == '3':
-                pass # 직접 완성
-            elif menu =='4':
+            elif menu == '2':
+                Contact.get_contact(clist)  # staticmethod는 클래스가 직접 접근함.
+            elif menu == '3':
+                name = input('삭제할 이름')
+                Contact.del_contact(clist, name)
+            elif menu == '4':
                 break
+
 
 if __name__ == '__main__':
     Contact.run()
-
 
 # """
 # python의 데코레이터는 함수형 2가지, class형 2가지 총 4가지 방식을 통해 작성할 수 있습니다.
@@ -74,7 +79,7 @@ if __name__ == '__main__':
 # 1) __init__메서드에 main함수(호출할 함수) 저장, self.function = function
 # 2) __call__(인스턴스를 함수 호출처럼 사용가능하게 만들어줌.)메서드에 호출할 함수의 매개변수를 넘긴다.
 # 3) 메인함수에 매개변수를 넣어서 호출하는 형태로 return
-# 4. Class 형 decorator(데코레이터에 인수가 있는 경우 O)
+# 4. Class 형 decorator(d데코레이터에 인수가 있는 경우 O)
 # 1) __init__메서드에 decorator의 인수 저장
 # 2) __call__ 메서드에 호출할 함수를 전달한다.
 # 3) 메인함수에 매개변수를 넣어서 호출하는 형태로 return
