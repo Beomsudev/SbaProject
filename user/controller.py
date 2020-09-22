50e17e0a74a95c80fcd3d9c2ddcc378973c5a1f7
Beomsudev <71618490+Beomsudev@users.noreply.github.com>
2020-09-22 11:59:24 +0900

    Add files via upload

---
+++ user/controller.py
@@ -0,0 +1,6 @@
+from entity import Entity
+from service import Service
+
+class Controller:
+    self.entity = Entity
+    self.service = Service
---
+++ user/service.py
@@ -0,0 +1,11 @@
+from entity import Entity
+
+"""
+
+feature.....
+
+"""
+
+class Service:
+    def __init__(self):
+        self.entity = Entity
