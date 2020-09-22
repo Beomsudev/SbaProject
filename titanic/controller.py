07614b08e0e8415cb422835c6ddfdc4d20c32084
Beomsudev <71618490+Beomsudev@users.noreply.github.com>
2020-09-22 11:58:28 +0900

    Add files via upload

--- titanic/controller.py
+++ titanic/controller.py
@@ -1,5 +1,21 @@
+from service import Service
+from entity import Entity
+
+
 class Controller:
     def __init__(self):
+        self.entity = Entity()
+        self.service = Service()
+
+    
+    def preprocessing(self):
         pass
 
-        
+    def modeling(self):
+        pass
+
+    def learning(self): # + evaluation
+        pass
+
+    def submit(self):   # 파일로 저장
+        pass    
